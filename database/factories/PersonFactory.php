2a709b36e6e092551f5858984f9bdb2879ce2734
Jac22 <jacopo.prudentino@gmail.com>
2023-02-06 14:16:08 +0100

    Salvati valori nella table con seeder

---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Person>
+ */
+class PersonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'firstName' => fake()->firstName(),
+            'lastName' => fake()->lastName(),
+            'dateOfBirth' => fake()->date(),
+            'height' => fake()->boolean() ?
+                        fake()->numberBetween(30, 250) :
+                        NULL,
+        ];
+    }
+}
